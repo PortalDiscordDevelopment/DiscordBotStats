32523a5711c4ab09ca6816658fc7855d2cdccb5d
Dummi <raydecrom@hotmail.com>
2023-06-01 21:30:27 +0200

    Made the bots as shards

---
+++ bots/ChannelManager.js
@@ -0,0 +1,9 @@
+import { Client } from '../main.js';
+import tokens from '../data/tokens.js';
+
+/**
+ ** Creates the instance of the bot.
+ */
+const client = new Client();
+
+client.login(tokens.ChannelManager);
--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,15 @@
 import { EventBuilder } from 'xernerx';
 
 export default class ReadyEvent extends EventBuilder {
-	constructor() {
-		super('ready', {
-			name: 'ready',
-		});
-	}
+    constructor() {
+        super('ready', {
+            name: 'ready',
+        });
+    }
 
-	async run(client) {
-		client.user.setPresence({
-			status: 'invisible',
-		});
-	}
+    async run(client) {
+        client.user.setPresence({
+            status: 'invisible',
+        });
+    }
 }
