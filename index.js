56059d399e0a33717982bd66ea2d6701738ac21c
TheDummi <raydegroot14@gmail.com>
2023-06-18 13:46:20 +0200

    1.0.1

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ const bots = Object.entries(tokens);
 /**
  ** Put the imported bots inside the array, order doesn't matter, the bot will take time to sign in by its server count.
  */
-// export const bots = [Amplifier, ChannelManager, Essence, Metamorphosis, RobloxUtilities, SupercellUtilities, PortalQuiz, YourApps, Zodiac];
 
-for (const [bot, token] of bots) {
-    if (!token) continue;
+(async () => {
+    for (const [bot, token] of bots) {
+        if (!token) continue;
 
-    console.info(`Starting ${bot}!`);
+        console.info(`Starting ${bot}!`);
 
-    new XernerxShardClient(`./bots/${bot}.js`, { token }, { log: { info: true } });
-}
+        const client = new XernerxShardClient(`./bots/${bot}.js`, { token, respawn: true }, { log: { info: true } });
+
+        await client;
+
+        console.info(`Done with ${bot}, loading next one...`);
+    }
+})();
