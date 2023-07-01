b7d87fd7695e9f8941f59f5365abe08a1043590a
TheDummi <raydegroot14@gmail.com>
2023-07-01 12:45:03 +0200

    cooldown thing

--- index.js
+++ index.js
@@ -4,20 +4,35 @@ import tokens from './data/tokens.js';
 
 const bots = Object.entries(tokens);
 
+const data = {};
+
 /**
  ** Put the imported bots inside the array, order doesn't matter, the bot will take time to sign in by its server count.
  */
 
 (async () => {
+    let i = 0;
+
     for (const [bot, token] of bots) {
+        i++;
+
         if (!token) continue;
 
         console.info(`Starting ${bot}!`);
 
-        const client = new XernerxShardClient(`./bots/${bot}.js`, { token, respawn: true }, { log: { info: true } });
+        let { shards, stats } = new XernerxShardClient(`./bots/${bot}.js`, { token, respawn: true }, { log: { info: true } });
 
-        await client;
+        shards = await shards;
 
-        console.info(`Done with ${bot}, loading next one...`);
+        const collector = await setInterval(() => {
+            if (shards.get(shards.size - 1).ready) {
+                clearInterval(collector);
+
+                data[bot] = stats;
+
+                if (i == bots.length) console.table(data);
+                else console.info(`Done with ${bot}, loading next one...`);
+            }
+        }, 1000);
     }
 })();
